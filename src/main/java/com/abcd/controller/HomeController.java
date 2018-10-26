dead09a0bd5975177537b536c54d1a256bade083
008 <saint_me@163.com>
2018-10-26 17:46:34 +0800

    dd

--- src/main/java/com/abcd/controller/HomeController.java
+++ src/main/java/com/abcd/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.abcd.controller;
 
+import com.abcd.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by YanMing on 2017/2/24.
@@ -17,5 +19,14 @@ public class HomeController {
     public String hello(){
         logger.info("the first jsp page");
         return "home";
+    }
+
+    @RequestMapping("/retUser")
+    @ResponseBody
+    public User retUser(){
+        User user=new User();
+        user.setName("小明");
+        user.setSex("男");
+        return user;
     }
 }
