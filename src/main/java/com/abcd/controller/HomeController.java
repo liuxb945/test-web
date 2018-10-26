69808c93206e923dbe75d4ee1c7c3fca7cfae6e3
008 <saint_me@163.com>
2018-10-26 17:25:05 +0800

    dd

---
+++ src/main/java/com/abcd/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.abcd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by YanMing on 2017/2/24.
+ */
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/hello")
+    public String hello(){
+        logger.info("the first jsp page");
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <h1>hello nihao</h1>
+    <p>This is the home page og this project</p>
+</head>
+<body>
+
+</body>
+</html>
